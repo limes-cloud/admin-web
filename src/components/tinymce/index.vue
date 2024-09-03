3e1c22bf3269136f9f647de82181d8a9147d4b2c
fangweiye <fangweiye@baidu.com>
2024-09-03 14:00:13 +0800

    feat: add mec file

--- src/components/tinymce/index.vue
+++ src/components/tinymce/index.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import { computed, onMounted, onUnmounted } from 'vue';
 
+import cryptoJs from 'crypto-js';
+
 import tinymce from 'tinymce/tinymce'; // tinymce核心文件
 import Editor from '@tinymce/tinymce-vue';
-
+import { PrepareUploadFile, UploadFile } from '@/api/resource/file/api';
 import 'tinymce/models/dom'; // 引入dom模块。从 tinymce6，开始必须有此模块导入
 import 'tinymce/themes/silver'; // 默认主题
 import 'tinymce/icons/default'; // 引入编辑器图标icon，不引入则不显示对应图标
@@ -41,6 +43,11 @@ import 'tinymce/plugins/template'; // 内容模板
 import 'tinymce/plugins/visualblocks'; // 显示元素范围
 import 'tinymce/plugins/visualchars'; // 显示不可见字符
 import 'tinymce/plugins/wordcount'; // 字数统计
+import { PrepareUploadFileReply } from '@/api/resource/file/type';
+import { AxiosResponse } from 'axios';
+import { useAppStore } from '@/store';
+
+const appStore = useAppStore();
 
 const props = defineProps({
 	modelValue: {
@@ -60,8 +67,13 @@ const props = defineProps({
 	id: {
 		type: [String, Number],
 		default: 'mytinymce'
+	},
+	uploadPath: {
+		type: String,
+		default: '/tinymec'
 	}
 });
+const imageHost = ref('');
 
 const emit = defineEmits(['update:modelValue']);
 
@@ -81,6 +93,7 @@ const initOptions = {
 		'body{font-size:14px;border-radis:3px;font-family:Microsoft YaHei,微软雅黑,宋体,Arial,Helvetica,sans-serif;line-height:1.5}img {max-width:100%;}',
 	height: props.height,
 	menubar: 'file edit view insert format tools table help',
+	promotion: false,
 	menu: {
 		file: {
 			title: 'File',
@@ -122,19 +135,145 @@ const initOptions = {
 	// 如需ajax上传可参考https://www.luweipai.cn/html/1670332196/
 	images_file_types: 'jpeg,jpg,png,gif,bmp',
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	images_upload_handler: (blobInfo, progress) =>
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		new Promise((resolve, reject) => {
-			const img = `data:image/jpeg;base64,${blobInfo.base64()}`;
-			resolve(img);
-		}),
+	images_upload_handler: async (blobInfo, progress) => {
+		const getUrlHost = (url: string): string => {
+			try {
+				const urlObj = new URL(url);
+				return urlObj.host;
+			} catch (error) {
+				return '';
+			}
+		};
 
-	placeholder: '在这里输入文字',
+		// 读取二进制数据
+		const readBinary = (file: File) => {
+			return new Promise((resolve, reject) => {
+				const fileReader = new FileReader();
+				fileReader.onload = (e: ProgressEvent<FileReader>): void => {
+					resolve(e.target?.result);
+				};
+				fileReader.onerror = (error) => {
+					reject(error);
+				};
+				fileReader.readAsArrayBuffer(file);
+			});
+		};
+
+		// 获取预上传参数
+		const getPrepareUploadReq = async (data: any, file: File) => {
+			const wordArray = cryptoJs.lib.WordArray.create(data);
+			const hash = cryptoJs.MD5(wordArray).toString();
+			const { name } = file;
+			return {
+				directoryPath: props.uploadPath,
+				name,
+				sha: hash,
+				size: Math.ceil(file.size / 1024)
+			};
+		};
+
+		// 执行上传
+		const handleUpload = (info: PrepareUploadFileReply, binary: ArrayBuffer) => {
+			return new Promise((resolve, reject) => {
+				const count = info.chunkCount as number;
+				const size = (info.chunkSize as number) * 1024;
+				const pArrr: Promise<AxiosResponse<any, any>>[] = [];
+				const uploaed = info.uploadChunks;
+				if (count <= 1) {
+					const formData = new FormData();
+					formData.append('data', new Blob([binary]));
+					formData.append('uploadId', info.uploadId as string);
+					formData.append('index', String(1));
+					pArrr.push(UploadFile(formData));
+				} else {
+					for (let i = 0; i < count; i += 1) {
+						// eslint-disable-next-line no-continue
+						if (uploaed && uploaed.includes(i + 1)) continue;
+						let data: ArrayBuffer = new ArrayBuffer(0);
+						if ((i + 1) * size > binary.byteLength) {
+							data = binary.slice(i * size, binary.byteLength);
+						} else {
+							data = binary.slice(i * size, (i + 1) * size);
+						}
+
+						const formData = new FormData();
+						formData.append('data', new Blob([data]));
+						formData.append('uploadId', info.uploadId as string);
+						formData.append('index', String(i + 1));
+						pArrr.push(UploadFile(formData));
+					}
+				}
+				pArrr.forEach((fn: Promise<AxiosResponse<any, any>>, index: number) => {
+					fn
+						.then((res) => {
+							progress(Math.ceil((index + 1) / pArrr.length));
+							if ((index + 1) / pArrr.length) {
+								resolve(res.data.url);
+							}
+						})
+						.catch((res) => {
+							reject(res);
+						});
+				});
+			});
+		};
+
+		return new Promise((resolve, reject) => {
+			// eslint-disable-next-line no-shadow
+			const handler = async (resolve, reject) => {
+				try {
+					appStore.startLoading('图片上传中～');
+					progress(0);
+					// 获取文件二进制数据
+					const file = blobInfo.blob() as File;
+					const binary = (await readBinary(file)) as ArrayBuffer;
+
+					// 进行预上传
+					const params = await getPrepareUploadReq(binary, file);
+					const { data } = await PrepareUploadFile(params);
+					// 触发秒传
+					if (data.uploaded) {
+						imageHost.value = getUrlHost(data.url as string);
+						progress(100);
+						resolve(data.url);
+					} else {
+						// 处理上传逻辑
+						const url = await handleUpload(data, binary);
+						imageHost.value = getUrlHost(url as string);
+
+						resolve(url);
+					}
+				} catch (error) {
+					reject(error);
+				} finally {
+					appStore.stopLoading();
+				}
+			};
+			handler(resolve, reject);
+		});
+	},
+	placeholder: '在这里输入内容',
 	branding: false, // tiny技术支持信息是否显示
 	statusbar: false, // 最下方的元素路径和字数统计那一栏是否显示
 	elementpath: false, // 元素路径是否显示
 	custom_undo_redo_levels: 10, // 撤销和重做的次数
-	draggable_modal: true // 对话框允许拖拽
+	draggable_modal: true, // 对话框允许拖拽
+	setup(editor) {
+		editor.on('NodeChange', (e) => {
+			if (e.element.nodeName.toLowerCase() === 'img') {
+				// 检查图片是否有自定义属性
+				const src = e.element.getAttribute('src');
+				if (src.indexOf(imageHost.value) !== -1) {
+					const arr = src.split('/');
+					const filename = arr[arr.length - 1];
+					const tmp = filename.split('.');
+					const md5 = tmp[0];
+					e.element.setAttribute('data-origin', 'resource');
+					e.element.setAttribute('data-md5', md5);
+				}
+			}
+		});
+	}
 };
 
 onMounted(async () => {
